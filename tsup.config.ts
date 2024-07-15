46044ecd0ec98f6819a4e2695a5749a34aae0a3b
Jeason <me@cowpoke.cc>
2024-07-15 11:05:06 +0800

    chore: update details

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig([
   {
     dts: true,
     entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
+    format: 'esm',
     sourcemap: true,
     esbuildPlugins: [wasmLoader({ mode: 'embedded' })],
   },
